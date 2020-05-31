4b367acd14f817d2c3257d6f47672b3e79946575
Emrah Bayar <emrah.bayar.98@gmail.com>
2020-06-01 00:32:30 +0300

    Second Commit

---
+++ src/main/java/app/controller/UserDetailsController.java
@@ -0,0 +1,20 @@
+package app.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userDetails")
+@CrossOrigin("*")
+public class UserDetailsController
+{
+    @PostMapping
+    public UserDetails get(Authentication authentication)
+    {
+        return (UserDetails) authentication.getPrincipal();
+    }
+}
